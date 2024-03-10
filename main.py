47896ecd725bfc5fb3e2bce129d69ce67e8ac725
Chris Nielsen <chris.rahbek.nielsen@gmail.com>
2024-03-10 13:52:51 +0100

    Basic functionalty finisched. Tasks can be added and deleted. Debugging not done. gitignorefile was added

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.list import TwoLineAvatarIconListItem, ILeftBody
+from kivymd.uix.selectioncontrol import MDCheckbox
+
+
+
 from datetime import datetime
 
 
@@ -22,13 +27,35 @@ class DialogContent(MDBoxLayout):
         date = value.strftime("%A %d %B %Y")
         self.ids.date_text.text = str(date)
 
+# Class for marking and deleteing the list item
+class ListItemWithCheckbox(TwoLineAvatarIconListItem):
+    def __init__(self, pk=None, **kwargs):
+        super().__init__(**kwargs)
+        self.pk = pk
+    
+    # Marking the item as complete or incomplete
+    def mark(self, check, the_list_item):
+        if check.active == True:
+            the_list_item.text = '[s]' + the_list_item.text + '[/s]'
+        else:
+            pass
+    
+    # Deleting the list item
+    def delete_item(self, the_list_item):
+        self.parent.remove_widget(the_list_item)
+
+
+class LeftCheckbox(ILeftBody, MDCheckbox):
+    pass
+    
+        
 # This is the main App class
 class MainApp(MDApp):
     # Flag
     task_list_dialog = None
     # This is the build function for setting the theme
     def build(self):
-        self.theme_cls.primary_palette = ("Teal")
+        self.theme_cls.primary_palette = ("Orange")
     
     # This is the show task function
     def show_task_function(self):
@@ -43,6 +70,9 @@ class MainApp(MDApp):
     # Adding tasks
     def add_task(self, task, task_date):
         print(f'{task.text} due on {task_date}')
+        self.root.ids['container'].add_widget(ListItemWithCheckbox(text = '[b]' + task.text + '[/b]', secondary_text = task_date))
+        task.text = ''
+
 
     # This is a dialog closing function
     def close_dialog(self, *args):
